9f98bb412002fcc7f3d414b7b4e89526058ff5fd
KaustavSaha04 <124427676+KaustavSaha04@users.noreply.github.com>
2024-05-29 18:55:33 +0530

    Add files via upload

---
+++ Rudoplhandcuttherope.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    unsigned int t;
+    cin>>t;
+    for(unsigned int i=0;i<t;i++)
+    {
+      unsigned int c=0;
+      unsigned int nails;
+      cin>>nails;
+      int data[nails][2];
+      for(unsigned int j=0;j<nails;j++)
+      {
+         cin>>data[j][0];
+         cin>>data[j][1];         
+      }
+      for(unsigned int j=0;j<nails;j++)
+      {
+         if(data[j][0]>data[j][1])
+           c++;
+      }    
+      cout<<c<<endl;
+      c=0;  
+    }
+    return(0);
+}
+
