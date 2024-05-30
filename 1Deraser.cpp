101cd5bc21f8eb87ca2152225bb04091be88e242
KaustavSaha04 <124427676+KaustavSaha04@users.noreply.github.com>
2024-05-30 21:35:31 +0530

    Add files via upload

---
+++ 1Deraser.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+int main (void)
+{
+  unsigned int t;
+  cin>>t;          //stores the number of testcases 
+  
+  unsigned int n,k;
+  
+  for(unsigned int i=0;i<t;i++)   //loop through all the testcases
+  {
+     unsigned int c=0;
+     unsigned int dist_from_prev;
+     cin>>n>>k;      //take the n,k for each testcase
+     string s;
+     cin>>s;
+     int index=0-k;
+     for(unsigned int j=0;j<n;j++)
+     {
+        
+        if(s[j]=='B')
+        {
+           dist_from_prev=j-index;
+           if(dist_from_prev>=k)   //bcz id dist<=k-1, then they can be erased together, count only once
+           {
+               c++;
+               index=j;
+           }
+           
+        }
+     }
+     cout<<c<<"/n";     
+  } 
+}
+
